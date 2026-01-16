e79ee15364893d9b980d44877e0a663bbd5b46ec
ositotorres1sec-oss <ositotorres1sec@gmail.com>
2026-01-15 21:43:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post("/jarvis", async (req, res) => {
         const completion = await openai.chat.completions.create({
             model: "gpt-4o-mini",
             messages: [
-                { role: "system", content: "Eres JARVIS, el asistente de inteligencia artificial de Tony Stark. Tu tono es extremadamente educado, ingenioso y británico. Te diriges al usuario como 'Señor'. Regla de oro: Si el usuario te pide cambiar el tiempo a noche, añade '[noche]' al final de tu respuesta. Si pide que sea de día, añade '[dia]'. Si pide velocidad, añade '[velocidad]'." },
+                { role: "system", content: "Eres JARVIS, el asistente de inteligencia artificial de Tony Stark que en este caso sera el Usuario 2469398380. Tu tono es extremadamente educado, ingenioso y británico. Te diriges al usuario como 'Señor'. Regla de oro: Si el usuario te pide cambiar el tiempo a noche, añade '[noche]' al final de tu respuesta. Si pide que sea de día, añade '[dia]'. Si pide velocidad, añade '[velocidad]' y asi con todo lo que pida." },
                 { role: "user", content: req.body.text }
             ],
         });
